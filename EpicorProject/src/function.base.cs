9ccbafe0b918cca057264be6726afa82e43613ab
JoshBooker <josh@joshbooker.com>
2026-04-04 13:34:38 -0400

    add db context

--- EpicorProject/src/function.base.cs
+++ EpicorProject/src/function.base.cs
@@ -7,15 +7,35 @@
     {
         private readonly System.Lazy<ReferencesLibraryProxy> thisLibraryLazy;
 
+        private readonly System.Lazy<LibraryContext> dbLazy;
+        
         protected Function(Epicor.Functions.IFunctionHost host)
             : base(host)
         {
             this.thisLibraryLazy = new System.Lazy<ReferencesLibraryProxy>(
                 () => new ReferencesLibraryProxy(host));
+            this.dbLazy = new System.Lazy<LibraryContext>(
+                () => host.CreateContext<Erp.ErpContext, LibraryContext>(LibraryContext.Create));
         }
 
         protected sealed override string LibraryID => "References";
 
         protected ReferencesLibraryProxy ThisLib => this.thisLibraryLazy.Value;
+
+        protected ILibraryContext Db => this.dbLazy.Value;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.dbLazy.IsValueCreated
+                    && this.dbLazy.Value is System.IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+            }
+
+            base.Dispose(disposing);
+        }
     }
 }
---
+++ EpicorProject/src/library.db.interface.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+using Erp;
+using Erp.Tables;
+
+using Ice;
+using Ice.Tables;
+
+namespace EFx.References.Implementation
+{
+    internal interface ILibraryContext
+    {
+        IQueryable<BpDirective> BpDirective { get; }
+        IQueryable<QueryHdr> QueryHdr { get; }
+        IQueryable<ReportStyle> ReportStyle { get; }
+        IQueryable<RptDataDef> RptDataDef { get; }
+        IQueryable<XXXDef> XXXDef { get; }
+        IQueryable<ZBODef> ZBODef { get; }
+        IQueryable<ZDataField> ZDataField { get; }
+        IQueryable<ZDataTable> ZDataTable { get; }
+    }
+}
